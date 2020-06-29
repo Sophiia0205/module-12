17d584a9e72e8acae39e55ab4e817b7637b44f54
sophiia_khramtchenko <sonayrepnina@yahoo.com>
2020-06-29 18:04:37 +0300

    hw10

--- src/index.js
+++ src/index.js
@@ -1,43 +1,48 @@
+import items from './menu.json';
+import menuTemplate from './template/menu-data.hbs';
+
 import './styles.css';
-import '@pnotify/core/dist/BrightTheme.css';
-import { error } from '@pnotify/core';
-import fetchCountries from './script/fetchCountries.js';
-import debounce from 'lodash.debounce';
-import countryList from './template/country-item.hbs';
-import country from './template/modal-item.hbs';
+console.log(items);
+console.log(menuTemplate);
 
-let text = document.querySelector('.input_text');
+const ref = {
+  itemFeed: document.querySelector('.js-menu'),
+};
 
-window.addEventListener('keypress', e => {
-  if (e.keyCode == 13) {
-    e.preventDefault();
+buildItemFeed(items);
+
+function buildItemFeed(items) {
+  const markup = items.map(item => menuTemplate(item)).join('');
+
+  ref.itemFeed.insertAdjacentHTML('beforeend', markup);
+}
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const switchControl = document.querySelector('#theme-switch-control');
+const body = document.body;
+
+switchControl.addEventListener('change', e => {
+  if (e.target.checked) {
+    body.classList.remove('light-theme');
+    body.classList.add('dark-theme');
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    body.classList.remove('dark-theme');
+    body.classList.add('light-theme');
+    localStorage.setItem('theme', Theme.LIGHT);
   }
 });
 
-let search = async () => {
-  if (text.value === '') {
-    return;
-  }
-  let result = await fetchCountries(text.value);
-  document.querySelector('.js-result').innerHTML = '';
-  if (!result) {
-    showError('No Countries Found');
-  } else if (result.length > 10) {
-    showError('Too many matches found. Please enter a more specific query!');
-  } else if (result.length === 1) {
-    document.querySelector('.js-result').innerHTML = country(result[0]);
-  } else {
-    document.querySelector('.js-result').innerHTML = countryList(result);
-  }
-};
-const showError = text => {
-  error({
-    text: text,
-    closer: false,
-    sticker: false,
-    destroy: true,
-    delay: 500,
-  });
-};
+const currentTheme = localStorage.getItem('theme');
+// console.log(currentTheme);
+if (currentTheme) {
+  body.classList = currentTheme;
 
-text.addEventListener('input', debounce(search, 500));
+  if (currentTheme === Theme.DARK) {
+    switchControl.checked = true;
+  }
+}
